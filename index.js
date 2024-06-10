8458ee6e36226aebdc587530f120e58c68c1482c
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-06-10 12:22:44 +0200

    "Added express-async-error package, refactored index.js to include CRUD operations for todos, and updated package.json and package-lock.json accordingly."

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 // npm i express-async-error
 // npm i sequelize sqlite3
 
-require("dotenv").config();
-
 const PORT = process.env?.PORT || 8000;
 const HOST = process.env?.HOST || "127.0.0.1";
 
 const express = require("express");
 const app = express();
+require("express-async-error");
+require("dotenv").config();
 
 //* All responses only to "/"
 app.all("/", (req, res) => {
@@ -18,9 +18,9 @@ app.all("/", (req, res) => {
 });
 
 //* Use responses to all routes except "/"
-app.use("/todo", (req, res) => {
-  res.send("TODO APP");
-});
+// app.use("/todo", (req, res) => {
+//   res.send("TODO APP");
+// });
 
 //* json to obj and obj to json
 app.use(express.json());
@@ -87,7 +87,90 @@ sequelize
   })
   .catch((err) => console.log(`Todo DB not connected`, err));
 
+//* ROUTER
+const router = express.Router();
+app.use(router);
+
+//* Middleware
+// Middleware to validate request body
+const validateTodoBody = (req, res, next) => {
+  // const { title, description, priority, isDone } = req.body;
+  const allowedKeys = ["title", "description", "priority", "isDone"];
+  const keys = Object.keys(req.body);
+
+  const isValid = keys.every((key) => allowedKeys.includes(key));
+
+  if (!isValid) {
+    return res.status(403).send({
+      errorCode: 403,
+      message: "Invalid request body",
+    });
+  }
+  next();
+};
+
 //* CRUD Operations
+
+// LIST all todos
+router.get("/todos", async (req, res) => {
+  try {
+    const todos = await TODO.findAll();
+    if (!todos) {
+      res.status(404).send({
+        error: true,
+        errorCode: res.statusCode,
+        message: "There is no todo item",
+      });
+    }
+    res.status(200).send({
+      error: false,
+      status: true,
+      data: todos,
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+// CREATE todo
+router.post("/todos", validateTodoBody, async (req, res) => {
+  try {
+    const newTodo = await TODO.create(req.body);
+    res.status(201).send({
+      error: false,
+      status: true,
+      data: newTodo,
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+// READ todo (Single todo)
+router.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    // const todo = await TODO.findOne({where: {id: req.params.id}});
+    const todo = await TODO.findByPk(id);
+    if (!todo) {
+      res.status(404).send({
+        error: true,
+        errorCode: res.statusCode,
+        message: `Todo with id number ${id} was not found`,
+      });
+    }
+    res.send({
+      error: false,
+      status: true,
+      data: todo,
+    });
+  } catch (err) {
+    throw new Error(err.message);
+  }
+});
+
+// UPDATE todo
+// DELETE todo
 
 //* error control
 const errorHandler = (err, req, res, next) => {
@@ -100,5 +183,7 @@ const errorHandler = (err, req, res, next) => {
     // stack: err.stack,
   });
 };
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server runned http://${HOST}:${PORT}`));
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 - connect db
 - create DB and table / model
 - CRUD operations
+  - /todos = method: GET => list all todos
+  - ...
